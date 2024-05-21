1cc678a11225cd5071db69fdf899c7afb981d537
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-21 11:56:10 +0200

    feat: add array sort

--- public/index.php
+++ public/index.php
@@ -1,20 +1,19 @@
 <?php
 require __DIR__ . './../vendor/autoload.php';
 
-$users = [
-    ['name' => 'Alice', 'age' => 25],
-    ['name' => 'Bob', 'age' => 30],
-    ['name' => 'Charlie', 'age' => 22],
-    ['name' => 'Diana', 'age' => 28],
-    ['name' => 'Di', 'age' => 15],
-    ['name' => 'Eve', 'age' => 35]
-];
-
-function adult($user){
-    return $user['age'] > 18;
-};
+$randomNumbers = [5, 3, 9, 1, 8, 7, 2, 6, 4, 0];
 
 print_r('<pre>');
-print_r(array_filter($users, 'adult'));
+print_r($randomNumbers);
+
+asort($randomNumbers);
+
+print_r('<pre>');
+print_r($randomNumbers);
+
+rsort($randomNumbers);
+
+print_r('<pre>');
+print_r($randomNumbers);
 
 ?>
