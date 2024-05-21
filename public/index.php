b84fe8819ece6c1f4aef069bfa2aeea7541dd1c7
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-21 13:57:45 +0200

    feat: add deleting

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 require __DIR__ . './../vendor/autoload.php';
 
 $textArray = [
@@ -11,7 +14,34 @@ $textArray = [
 
 $numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+$new_arr = array_merge($textArray,$numbersArray);
+
 print_r('<pre>');
-print_r(array_merge($textArray,$numbersArray));
+print_r($new_arr);
+
+unset($new_arr[10]);
+
+echo 'Массив после unset()';
+print_r('<pre>');
+print_r($new_arr);
+
+$removed = array_splice($new_arr, 3,6);
+
+echo 'Массив после array_splice()';
+print_r('<pre>');
+print_r($new_arr);
+print_r($removed);
+
+echo 'Массив после array_shift()';
+print_r('<pre>');
+print_r(array_shift($new_arr));
+echo '<br>';
+print_r($new_arr);
+
+echo 'Массив после array_pop()';
+print_r('<pre>');
+print_r(array_pop($new_arr));
+echo '<br>';
+print_r($new_arr);
 
 ?>
