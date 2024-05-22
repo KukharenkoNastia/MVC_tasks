961edb428839c857a14d118ad0202f2ec7fdd97e
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-22 12:59:20 +0200

    feat: add algorithm

--- public/index.php
+++ public/index.php
@@ -4,43 +4,97 @@ declare(strict_types=1);
 
 require __DIR__ . './../vendor/autoload.php';
 
-$sales = [
-    ['product' => 'apple', 'quantity' => 10, 'price' => 1.5],
-    ['product' => 'banana', 'quantity' => 5, 'price' => 1.0],
-    ['product' => 'orange', 'quantity' => 8, 'price' => 1.2],
-    ['product' => 'apple', 'quantity' => 3, 'price' => 1.5],
-    ['product' => 'banana', 'quantity' => 7, 'price' => 1.0],
-    ['product' => 'orange', 'quantity' => 2, 'price' => 1.2],
-    ['product' => 'apple', 'quantity' => 6, 'price' => 1.5],
-];
+echo 'Массив до сортировки пузырьком';
 
-//Используйте функции array_filter(), array_count_values(), array_sum() or array_reduce()
-
-echo 'Информация по яблокам';
-
-function infoApple($el):bool
+function generateArray(): array
 {
-    return $el['product'] == 'apple';
+    $numbers = [];
+    for ($i = 0; $i < 10; $i++) {
+        $numbers[] = rand(1, 50);
+    }
+    return $numbers;
 }
 
-print_r('<pre>');
-print_r(array_filter($sales, 'infoApple'));
-
-echo 'Продано проуктов по видам';
+$numbers = generateArray();
 
 print_r('<pre>');
-print_r(array_count_values(array_column($sales, 'product')));
+print_r($numbers);
 
-echo 'Всего продуктов';
+for ($i = 0; $i < count($numbers); $i++) {
+    for ($n = 0; $n < count($numbers) - $i - 1; $n++) {
+        if ($numbers[$n] > $numbers[$n + 1]) {
+            $temp = $numbers[$n];
+            $numbers[$n] = $numbers[$n + 1];
+            $numbers[$n + 1] = $temp;
+        }
+    }
+}
+
+echo 'Массив после сортировки пузырьком';
+print_r('<pre>');
+print_r($numbers);
+
+echo 'Массив до сортировки слиянием';
+
+function generateArrayMegre(): array
+{
+    $numbers_merge = [];
+    for ($i = 0; $i < 11; $i++) {
+        $numbers_merge[] = rand(1, 50);
+    }
+    return $numbers_merge;
+}
+
+$numbers_merge = generateArrayMegre();
 
 print_r('<pre>');
-print_r(array_sum(array_column($sales, 'quantity')));
+print_r($numbers_merge);
 
-echo '<br>';
-echo 'Итого сумма';
+function sortArr($arr): array
+{
+    if (count($arr) <= 1) {
+        return $arr;
+    }
 
-print_r('<pre>');
-print_r(array_reduce($sales, function($sum, $sales){
-    $sum += $sales['quantity']*$sales['price'];
-    return $sum;
-}));
+    $middle = (int)floor(count($arr) / 2);
+    $arr1 = array_slice($arr, 0, $middle);
+    $arr2 = array_slice($arr, $middle);
+
+    $arr1 = sortArr($arr1);
+    $arr2 = sortArr($arr2);
+
+    return sortMerge($arr1, $arr2);
+}
+
+function sortMerge($arr1, $arr2):array
+{
+    $result = [];
+    $ind1 = 0;
+    $ind2 = 0;
+
+
+    while ($ind1 < count($arr1) && $ind2 < count($arr2)) {
+        if ($arr1[$ind1] <= $arr2[$ind2]) {
+            $result[] = $arr1[$ind1];
+            $ind1++;
+        } else {
+            $result[] = $arr2[$ind2];
+            $ind2++;
+        }
+    }
+
+    while ($ind1 < count($arr1)) {
+        $result[] = $arr1[$ind1];
+        $ind1++;
+    }
+
+    while ($ind2 < count($arr2)) {
+        $result[] = $arr2[$ind2];
+        $ind2++;
+    }
+
+    return $result;
+}
+
+echo 'Массив после сортировки слиянием';
+print_r(sortArr($numbers_merge));
