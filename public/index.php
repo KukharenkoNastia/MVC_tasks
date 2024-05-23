aa160d6164d3ccbe4affcd140afbe5d430968369
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-23 11:22:03 +0200

    feat: add str sort and compare

--- public/index.php
+++ public/index.php
@@ -4,31 +4,37 @@ declare(strict_types=1);
 
 require __DIR__ . './../vendor/autoload.php';
 
-$dates = [
-    '22.05.2024',
-    '2024-05-22',
-    '2024-05-22T14:30:00Z',
-    '22nd May 2024',
-    'Wednesday, May 22, 2024',
-    '2024-05-22 14:30:00',
-    '2023-12-31',
-    '2025-01-01',
-    '2024/05/22'
-];
+$str1 = 'some string';
+$str2 = 'sOme strIng';
 
-function encoder($str):string
+function strCompare($str1, $str2): string
 {
-    return base64_encode($str);
-}
-function decoder($str):string
-{
-    return base64_decode($str);
+    if (strcasecmp($str1, $str2) == 0) {
+        $format = 'Строки %s и %s эквивалентны без учета регистра';
+        $res = sprintf($format, $str1, $str2);
+    } else {
+        $format = 'Строки %s и %s не эквивалентны';
+        $res = sprintf($format, $str1, $str2);
+    }
+    return $res;
 }
 
-foreach ($dates as $date) {
-    $code = encoder($date);
-    echo encoder($date);
-    echo '<br>';
-    echo decoder($code);
-    echo '<br>';
+echo strCompare($str1, $str2);
+
+$strArr = ['img12.png', 'img10.png', 'img2.png', 'img1.png'];
+
+function strnatcmpString($strArr): array
+{
+    usort($strArr, "strnatcmp");
+    return $strArr;
 }
+
+function sortString($strArr): array
+{
+    sort($strArr);
+    return $strArr;
+}
+
+print_r('<pre>');
+print_r(strnatcmpString($strArr));
+print_r(sortString($strArr));
