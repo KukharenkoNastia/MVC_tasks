026c50bd2190197706c2af428d5cc7d201a5dbfe
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-29 20:20:59 +0200

    fix: refactoring code

--- public/index.php
+++ public/index.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 require __DIR__ . './../vendor/autoload.php';
 
 $textArray = [
-    "      Hello, world!  ",
-    "    Welcome to PHP!",
-    "   Массивы в PHP очень гибкие!  ",
-    "  Этот массив содержит строки текста!",
-    "   Желаю вам хорошего дня!"
+    '      Hello, world!  ',
+    '    Welcome to PHP!',
+    '   Массивы в PHP очень гибкие!  ',
+    '  Этот массив содержит строки текста!',
+    '   Желаю вам хорошего дня!'
 ];
 
-function addEnd($el):string
+function addEnd(string $el): string
 {
-    return $el.'! The best!';
+    $format = '%s! The best!';
+    return sprintf($format, $el);
 }
 
 echo 'Массив после array_map()';
@@ -22,42 +23,38 @@ echo '<br>';
 print_r('<pre>');
 print_r(array_map('addEnd', $textArray));
 
-function trim_el($el):string
+function trim_el(string $el): string
 {
     return trim($el);
 }
 
 echo 'Массив после trim()';
 echo '<br>';
-print_r('<pre>');
 print_r(array_map('trim_el', $textArray));
 
-function string_up($el):string
+function string_up(string $el): string
 {
     return strtoupper($el);
 }
 
 echo 'Массив после strtoupper()';
 echo '<br>';
-print_r('<pre>');
 print_r(array_map('string_up', $textArray));
 
-function string_low($el):string
+function string_low(string $el): string
 {
     return strtolower($el);
 }
 
 echo 'Массив после strtolower()';
 echo '<br>';
-print_r('<pre>');
 print_r(array_map('string_low', $textArray));
 
-function string_replace($el):string
+function string_replace(string $el): string
 {
     return str_replace('!', '...', $el);
 }
 
 echo 'Массив после str_replace()';
 echo '<br>';
-print_r('<pre>');
 print_r(array_map('string_replace', $textArray));
