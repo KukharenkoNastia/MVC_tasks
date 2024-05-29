8a0424b55fc9164a38b98217e600ac50a97e8902
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-29 19:47:49 +0200

    fix: refactoring code

--- public/index.php
+++ public/index.php
@@ -3,14 +3,14 @@
 require __DIR__ . './../vendor/autoload.php';
 
 $textArray = [
-    "Привет, мир!",
-    "Добро пожаловать в PHP.",
-    "Массивы в PHP очень гибкие.",
-    "Этот массив содержит строки текста.",
-    "Желаю вам хорошего дня!"
+    'Привет, мир!',
+    'Добро пожаловать в PHP.',
+    'Массивы в PHP очень гибкие.',
+    'Этот массив содержит строки текста.',
+    'Желаю вам хорошего дня!'
 ];
 
 $numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 print_r('<pre>');
-print_r(array_merge($textArray,$numbersArray));
+print_r(array_merge($textArray, $numbersArray));
