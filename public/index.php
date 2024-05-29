ac58454907eab790f79aa394e2f4e77dce9ab527
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-29 20:01:30 +0200

    fix: refactoring code

--- public/index.php
+++ public/index.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 require __DIR__ . './../vendor/autoload.php';
 
 $users = [
-    "alice" => "alice@example.com",
-    "bob" => "bob@example.com",
-    "charlie" => "charlie@example.com",
-    "dave" => "dave@example.com",
-    "eve" => "eve@example.com",
-    "frank" => "frank@example.com",
-    "grace" => "grace@example.com",
-    "heidi" => "heidi@example.com",
-    "ivan" => "ivan@example.com",
-    "judy" => "judy@example.com",
+    'alice' => 'alice@example.com',
+    'bob' => 'bob@example.com',
+    'charlie' => 'charlie@example.com',
+    'dave' => 'dave@example.com',
+    'eve' => 'eve@example.com',
+    'frank' => 'frank@example.com',
+    'grace' => 'grace@example.com',
+    'heidi' => 'heidi@example.com',
+    'ivan' => 'ivan@example.com',
+    'judy' => 'judy@example.com',
 ];
 
 $keys = array_keys($users);
