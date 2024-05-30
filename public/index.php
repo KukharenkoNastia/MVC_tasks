f555fb638b98ea1a907a10e7fb10b5b9c495be86
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-30 10:43:27 +0200

    fix: refactoring code

--- public/index.php
+++ public/index.php
@@ -4,33 +4,36 @@ declare(strict_types=1);
 
 require __DIR__ . './../vendor/autoload.php';
 
-function replaceString($input):string {
+function replaceString(string $input): string
+{
     return str_replace('e', 'EEE', $input);
 }
 
-function replaceRegString($input):string {
+function replaceRegString(string $input): string
+{
     return preg_replace('/[^\w]/', '', $input);
 }
 
-function position($input):int
+function position(string $input): int
 {
     return strpos($input, 'e');
 }
 
 $strings = [
-    "  Hello,   world!  ",
-    "This    is    a test. ",
-    "    PHP is  great!!! ",
-    " Clean   this   string...  ",
-    "\tWhitespace\tand\nnewlines\n",
+    '  Hello,   world!  ',
+    'This    is    a test. ',
+    '    PHP is  great!!! ',
+    ' Clean   this   string...  ',
+    '\tWhitespace\tand\nnewlines\n',
 ];
 
-foreach ($strings as $str)
-{
+foreach ($strings as $str) {
     echo $str;
     echo '<br>';
+
     echo replaceString($str);
     echo '<br>';
-    echo 'Элемент е найден в строке '.replaceRegString($str).' на месте '.position(replaceRegString($str));
+
+    echo 'Элемент е найден в строке ' . replaceRegString($str) . ' на месте ' . position(replaceRegString($str));
     echo '<br>';
 }
