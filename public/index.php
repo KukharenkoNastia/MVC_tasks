8cf8ccd796333ac863f3eae5e8f84bf97788ba8f
khr_nastasia <nastassia.kukharenka@innowise.com>
2024-05-30 13:01:45 +0200

    fix: refactoring code

--- public/index.php
+++ public/index.php
@@ -7,29 +7,24 @@ require __DIR__ . './../vendor/autoload.php';
 $str1 = 'some string';
 $str2 = 'sOme strIng';
 
-function strCompare($str1, $str2): string
+function strCompare(string $str1, string $str2): string
 {
-    if (strcasecmp($str1, $str2) == 0) {
-        $format = 'Строки %s и %s эквивалентны без учета регистра';
-        $res = sprintf($format, $str1, $str2);
-    } else {
-        $format = 'Строки %s и %s не эквивалентны';
-        $res = sprintf($format, $str1, $str2);
-    }
-    return $res;
+    strcasecmp($str1, $str2) === 0 ? $str = 'эквивалентны без учета регистра' : $str = 'не эквивалентны';
+    $format = 'Строки %s и %s %s';
+    return sprintf($format, $str1, $str2, $str);
 }
 
 echo strCompare($str1, $str2);
 
 $strArr = ['img12.png', 'img10.png', 'img2.png', 'img1.png'];
 
-function strnatcmpString($strArr): array
+function strnatcmpString(array $strArr): array
 {
     usort($strArr, "strnatcmp");
     return $strArr;
 }
 
-function sortString($strArr): array
+function sortString(array $strArr): array
 {
     sort($strArr);
     return $strArr;
